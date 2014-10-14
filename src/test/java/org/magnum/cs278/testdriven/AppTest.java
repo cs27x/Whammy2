945f3579a62d3e8d94d26384b6e53dac803a1cb9
Brendan McNamara <bahai9@live.com>
2014-10-14 11:51:40 -0500

    Adds test for getEventsForLocation

--- src/test/java/org/magnum/cs278/testdriven/AppTest.java
+++ src/test/java/org/magnum/cs278/testdriven/AppTest.java
@@ -2,6 +2,8 @@ package org.magnum.cs278.testdriven;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -11,5 +13,24 @@ public class AppTest {
 		@Test
 		public void testNothing() {
 			assertTrue(true);
+		}
+		
+		
+		@Test
+		public void testGetEventsWithLocation() throws Exception {
+			String location = "Riverfront Park/Court of Flags";
+			// There are exactly 2 events with this
+			// location.
+			int count = 2;
+			
+			List<Event> events = app.getEventsWithLocation(location);
+			
+			assertEquals(count, events.size());
+			
+			// Make sure that these are the events
+			// that we expect
+			for (Event event : events) {
+				assertEquals(location, event.getLocation());
+			}
 		}
 }
