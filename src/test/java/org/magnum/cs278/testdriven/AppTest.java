47adc14aa4695bf4f7ac6fa011de7fd2ef3db410
Brendan McNamara <bahai9@live.com>
2014-10-14 11:22:58 -0500

    Adds test package

---
+++ src/test/java/org/magnum/cs278/testdriven/AppTest.java
@@ -0,0 +1,15 @@
+package org.magnum.cs278.testdriven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+		private App app = new App();
+		
+		@Test
+		public void testNothing() {
+			assertTrue(true);
+		}
+}
